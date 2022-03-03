922a9220755ccf8f57debc88d37505f8236e053f
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2022-03-03 13:13:10 -0800

    can retreive items

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
-from typing import Optional
 from pydantic import BaseModel, HttpUrl
 import hashlib
 import base64
 import boto3
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
@@ -14,24 +13,50 @@ def put_url(short_url: bytes, created_at: str, long_url: str, dynamodb=None):
         dynamodb = boto3.resource("dynamodb", endpoint_url="http://db:8000")
 
     table = dynamodb.Table("Url")
-    response = table.put_item(Item={"short_url": short_url})
+    print(f"put short_url {short_url}")
+    response = table.put_item(
+        Item={"short_url": short_url, "created_at": created_at, "long_url": long_url}
+    )
     return response
 
 
-class Request(BaseModel):
+def get_url(short_url: bytes, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource("dynamodb", endpoint_url="http://db:8000")
+    table = dynamodb.Table("Url")
+    print(f"get short_url {short_url}")
+    response = table.get_item(Key={"short_url": short_url})
+    return response
+
+
+class PostRequest(BaseModel):
     long_url: HttpUrl
-    custom_url: str
+    custom_url: str | None = None
 
 
-@app.post("/short_url", status_code=201)
-def create_short_url(req: Request):
+class PostResponse(BaseModel):
+    long_url: HttpUrl
+    custom_url: str | None = None
+    short_url: str
+
+
+@app.post("/short_url", status_code=201, response_model_exclude_unset=True)
+def create_short_url(req: PostRequest):
     hash = hashlib.md5(req.long_url.encode("utf-8"))
     # TODO: trim ==
     short_url = base64.urlsafe_b64encode(hash.digest())
-    response = put_url(short_url, "time", req.long_url)
-    return response
+    put_url(short_url, "time", req.long_url)
+    return PostResponse(
+        long_url=req.long_url, custom_url=req.custom_url, short_url=short_url
+    )
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/short_url/{short_url}")
+def create_short_url(short_url: str = Path(..., example="x7kg9X5VPfK7aCcvYVcCEA==")):
+    short_url = short_url.encode("utf-8")
+    item = get_url(short_url)["Item"]
+    return {
+        "long_url": item["long_url"],
+        "created_at": item["created_at"],
+        "short_url": item["short_url"],
+    }
